09036fdd7c24fd40ee993d3153156de9cdb39eb3
liug <liugang@zbj.com>
2018-11-01 15:17:32 +0800

    1

---
+++ eurekaconfig/src/main/java/lg/eureka/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package lg.eureka.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author lg
+ * @date 2018-10-31
+ * &Desc config
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
